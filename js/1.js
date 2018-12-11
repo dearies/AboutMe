ddad981c06d60cd94d12bd2c001ae8931ddb8184
Bao Thanh <drthanhstvv@gmail.com>
2018-12-12 00:14:02 +0700

    adding jquery for menu

--- js/1.js
+++ js/1.js
@@ -1,3 +1,51 @@
 $(document).ready(function() {
-	
+	$(window).scroll(function () { 
+        var position = $('html,body').scrollTop();
+        if (position >= 300) {
+            $('nav').addClass('navChange');
+            $('.btn-gotop').addClass('gotop-change');
+        }
+        else {
+            $('nav').removeClass('navChange');
+            $('.btn-gotop').removeClass('gotop-change');
+        }
+    });
+    // scroll memu
+    $('header nav .right ul li:nth-child(1), .btn-gotop').click(function (e) { 
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0},1000);
+        $('header .right').removeClass('right-change');
+        $('header nav ul li').removeClass('lichange');
+    });
+    $('header nav .right ul li:nth-child(2)').click(function (e) { 
+        e.preventDefault();
+        $('html, body').animate({scrollTop: $('.about').offset().top},1000);
+        $('header .right').removeClass('right-change');
+        $('header nav ul li').removeClass('lichange');
+    });
+    $('header nav .right ul li:nth-child(3)').click(function (e) { 
+        e.preventDefault();
+        $('html, body').animate({scrollTop: $('.education').offset().top},1000);
+        $('header .right').removeClass('right-change');
+        $('header nav ul li').removeClass('lichange');
+    });
+    $('header nav .right ul li:nth-child(4)').click(function (e) { 
+        e.preventDefault();
+        $('html, body').animate({scrollTop: $('.skills').offset().top},1000);
+        $('header .right').removeClass('right-change');
+        $('header nav ul li').removeClass('lichange');
+    });
+    $('header nav .right ul li:nth-child(5)').click(function (e) { 
+        e.preventDefault();
+        $('html, body').animate({scrollTop: $('.contact').offset().top},1000);
+        $('header .right').removeClass('right-change');
+        $('header nav ul li').removeClass('lichange');
+    });
+
+    //btn-menu for phone
+    $('header .btn-menu').click(function (e) { 
+        e.preventDefault();
+        $('header .right').toggleClass('right-change');
+        $('header nav ul li').toggleClass('lichange');
+    });
 });
